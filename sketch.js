359f8a852d0029a65694cbc3e0c8042572b627d1
tangamandapio6 <00001104918821sp@al.educacao.sp.gov.br>
2024-05-10 17:59:28 -0300

    adiconando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+  canvas {
+    display: block;
+  }
